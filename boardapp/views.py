d571bc8a99c1f2d6ba131cf2617ac796f1a51715
sumiok <tap.miwo@gmail.com>
2022-12-27 06:26:46 +0900

    models

--- boardapp/views.py
+++ boardapp/views.py
@@ -1,7 +1,12 @@
 from django.shortcuts import render
+from django.contrib.auth.models import User
 
 # Create your views here.
+
 def signupfunc(request):
+  object = User.objects.get(username='admin')
+  object_list = User.object.all()
+  print(object.email)
   if request.method == "POST":
     print('this is post method')
   else:
