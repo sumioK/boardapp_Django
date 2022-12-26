1a4de1e550d3d1d751b3388d143183593d6a8b42
sumiok <tap.miwo@gmail.com>
2022-12-27 06:04:12 +0900

    defferenc of post and get

--- boardapp/views.py
+++ boardapp/views.py
@@ -2,4 +2,8 @@ from django.shortcuts import render
 
 # Create your views here.
 def signupfunc(request):
+  if request.method == "POST":
+    print('this is post method')
+  else:
+    print('this is not post method')
   return render(request, 'signup.html', {'some':100})
