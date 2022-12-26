39bbc1090aea0aa214f3528c8614a1db6b9b426e
sumiok <tap.miwo@gmail.com>
2022-12-27 05:20:55 +0900

    render

--- boardapp/urls.py
+++ boardapp/urls.py
@@ -1,7 +1,7 @@
-from django.contrib import admin
 from django.urls import path
+from .views import signupfunc
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    
+    path('signup/', signupfunc),
+
 ]
