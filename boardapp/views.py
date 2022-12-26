546af4e897558e7a1872cd50f5f9443716398537
sumiok <tap.miwo@gmail.com>
2022-12-27 06:53:38 +0900

    try except

--- boardapp/views.py
+++ boardapp/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 from django.contrib.auth.models import User
+from django.db import IntegrityError
 
 # Create your views here.
 
@@ -7,6 +8,11 @@ def signupfunc(request):
   if request.method == "POST":
     username = request.POST['username']
     password = request.POST['password']
-    # createが実行された時点で既にユーザーは作成保存される(saveメソッドは必ずしも必要ない)
-    user = User.objects.create_user(username, '', password)
-  return render(request, 'signup.html', {'some':100})
+    try:
+      # createが実行された時点で既にユーザーは作成保存される(saveメソッドは必ずしも必要ない)
+      user = User.objects.create_user(username, '', password)
+      return render(request, 'signup.html', {'some':500})
+    except IntegrityError:
+      return render(request,'signup.html', {'error': 'このユーザーは既に登録されています'})
+
+  return render(request, 'signup.html', {'some':500})
